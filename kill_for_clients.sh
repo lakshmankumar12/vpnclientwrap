ce223d54cf046eab252a46709bfbe2aa1aac1a2f
lakshman kumar <lakshmankumar@gmail.com>
2016-11-07 19:42:54 -0800

    replace resolv.conf

--- kill_for_clients.sh
+++ kill_for_clients.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Existing fort process"
 ps -ef | grep fort
@@ -6,3 +6,16 @@ echo "Killing them"
 ps -ef | grep fort | awk '{print $2} ' | xargs sudo kill -9
 echo "Rechecking"
 ps -ef | grep fort
+echo "Your /etc/resolv.conf is"
+echo "------------------------"
+cat /etc/resolv.conf
+echo "========================"
+echo "Tmp /etc/resolv.conf is"
+echo "------------------------"
+cat /tmp/resolv.conf.orig
+echo "========================"
+echo -n "Replace(y/n):"
+read repl
+if [[ "x$repl" -eq "xy" ]] ; then
+  sudo cp /tmp/resolv.conf.orig /etc/resolv.conf
+fi
